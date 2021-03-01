a81cefc30de3e60e5f2bb6e5449b04a3dfdb3c73
dlc2787 <54549285+dlc2787@users.noreply.github.com>
2021-02-28 20:29:49 -0500

    assignment complete
    all assignments components completed

--- src/responses.js
+++ src/responses.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
+const users = {};
+
 // form response to send
 const formResponse = (request, response, code, json) => {
   response.writeHead(code, { 'Content-Type': 'application/json' });
@@ -38,18 +40,44 @@ const getNotFoundMeta = (request, response) => {
   headResponse(request, response, 404);
 };
 
-//get users
-const getUsers = (request, response, users) => {
-  formResponse(request, response, 200, { message: users });
+// get users
+const getUsers = (request, response) => {
+  formResponse(request, response, 200, {users: users});
 };
 const getUsersMeta = (request, response) => {
   headResponse(request, response, 200);
 };
 
-//add users
-const addUsers = (request, response, user) => {
-    
-}
+// add users
+const addUser = (request, response, body) => {
+  const jsonresponse = {
+    message: 'Name and Age are both required',
+  };
+
+  if (!body.name || !body.age) {
+    jsonresponse.id = 'missingParams';
+    return formResponse(request, response, 400, jsonresponse);
+  }
+  console.log(body);
+
+  let responsecode = 201;
+
+  if (users[body.name]) {
+    responsecode = 204;
+  } else {
+    users[body.name] = {};
+    users[body.name].name = body.name;
+  }
+
+  users[body.name].age = body.age;
+
+  if (responsecode === 201) {
+    jsonresponse.message = 'Created Successfully!';
+    return formResponse(request, response, responsecode, jsonresponse);
+  }
+
+  return headResponse(request, response, responsecode);
+};
 
 module.exports = {
   getIndex,
@@ -58,4 +86,5 @@ module.exports = {
   getNotFoundMeta,
   getUsers,
   getUsersMeta,
+  addUser,
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const url = require('url');
-
+const query = require('querystring');
 const responses = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
@@ -13,20 +13,51 @@ const urlPair = {
     '/notReal': responses.getNotFound,
     notFound: responses.getNotFound,
   },
-  POST: {
-    //
-  },
   HEAD: {
     '/getUsers': responses.getUsersMeta,
     '/notReal': responses.getNotFoundMeta,
   },
 };
 
+const handleGet = (request, response, parsedURL) => {
+  if (urlPair[request.method][parsedURL.pathname]) {
+    urlPair[request.method][parsedURL.pathname](request, response);
+  } else {
+    urlPair.GET.notFound(request, response);
+  }
+};
+
+const handlePost = (request, response, parsedURL) => {
+  if (parsedURL.pathname === '/addUser') {
+    const body = [];
+    // handle errors
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    // load data chunks
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodystring = Buffer.concat(body).toString();
+      const queryparams = query.parse(bodystring);
+
+      responses.addUser(request, response, queryparams);
+    });
+  }
+};
+
 const onReq = (request, response) => {
   console.log(request.url);
   const parsedURL = url.parse(request.url);
-  if (urlPair[request.method][parsedURL.pathname]) {
-    urlPair[request.method][parsedURL.pathname](request, response, { users: '' });
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedURL);
+  } else {
+    handleGet(request, response, parsedURL);
   }
 };
 
